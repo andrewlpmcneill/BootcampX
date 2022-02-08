bd1b6f1f33c571cbc4bb7db95910639cfaef606f
Andrew McNeill <andrew@andrewmc.ca>
2022-02-07 22:37:54 -0400

    Completes BootcampX Queries 3 - Group By

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name AS cohort_name, COUNT(students.cohort_id) AS student_count
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING COUNT(students.cohort_id) >= 18
+ORDER BY COUNT(students.cohort_id);
